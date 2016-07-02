437d89ffdf551d6ff13f8615f0fdf13ce688972e
Judson Silva <judsonbarroso@gmail.com>
2016-07-01 22:22:55 -0300

    add new tests

--- test/insert.js
+++ test/insert.js
@@ -4,14 +4,47 @@ var _ = require('underscore');
 
 describe('AVL Tree', function() {
   describe('#insert', function () {
-    it('should return list of balanced values', function () {
+
+    it('rotate to left', function () {
 
       var tree = new AVLTree(),
-          values = [5,3,2,1,9,4,8,7],
-          balancedValues = [3,2,1,5,4,8,7,9],
+          values = [1,2,3],
+          balancedValues = [2,1,3],
           i = 0;
 
-      values.forEach(function(value){
+      _.each(values, function(value){
+        tree.insert(value);
+      });
+
+      tree.preOrder(function( node ){
+        assert.equal(node.value, balancedValues[i++]);
+      });
+    });
+
+    it('rotate to right', function () {
+
+      var tree = new AVLTree(),
+          values = [3,2,1],
+          balancedValues = [2,1,3],
+          i = 0;
+
+      _.each(values,function(value){
+        tree.insert(value);
+      });
+
+      tree.preOrder(function( node ){
+        assert.equal(node.value, balancedValues[i++]);
+      });
+    });
+
+    it('double rotate to left', function () {
+
+      var tree = new AVLTree(),
+          values = [3,1,2],
+          balancedValues = [2,1,3],
+          i = 0;
+
+      _.each(values,function(value){
         tree.insert(value)
       });
 
@@ -19,5 +52,39 @@ describe('AVL Tree', function() {
         assert.equal(node.value, balancedValues[i++]);
       });
     });
+
+    it('double rotate to right', function () {
+
+      var tree = new AVLTree(),
+          values = [1,3,2],
+          balancedValues = [2,1,3],
+          i = 0;
+
+      _.each(values,function(value){
+        tree.insert(value)
+      });
+
+      tree.preOrder(function( node ){
+        assert.equal(node.value, balancedValues[i++]);
+      });
+    });
+
+    it('aleatory sequence', function(){
+
+      var tree = new AVLTree(),
+          values = [300,400,350,325,315,320],
+          balancedValues = [325,315,300,320,350,400]
+          i = 0;
+
+      _.each(values,function(value){
+        tree.insert(value)
+      });
+
+      tree.preOrder(function( node ){
+        assert.equal(node.value, balancedValues[i++]);
+      });
+
+    });
+
   });
 });
